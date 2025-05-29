72a037ca4668e02d88294a44c1f79af0be351dca
lucas-schuster <lucas.schuster@gmail.com>
2025-05-29 17:05:16 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,13 @@ app.use('/api/survey', surveyRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/results', resultsRoutes);
 
+// No seu servidor backend
+const cors = require('cors');
 
+app.use(cors({
+  origin: ['https://neuro-pulse3.vercel.app', 'http://localhost:3000'], // adicione outros domínios se necessário
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
